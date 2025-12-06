aed2dcfa5345adb217f452f6a6a1354f57e7cd73
Julia Duffy <jtduffy7@gmail.com>
2025-12-06 15:55:16 -0700

    made a 'state' and 'state_list' route as well as a 'state' django model to fetch a list of states or a single state from the db

---
+++ states/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# probably shouldnt be using a string as a pk but I dont care
+class State(models.Model):
+    name = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=5)
+    capital = models.CharField(max_length=50)
+    flower = models.CharField(max_length=50)
+    bird = models.CharField(max_length=100)
+    description = models.TextField()
+    population = models.IntegerField()
+    nickname = models.CharField(max_length=100, null=True)
+    img = models.ImageField(upload_to="media/", null=True, blank=True)  
+    
+
---
+++ states/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,  get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import State
+from .serializers import StateSerializer
+
+@api_view(['GET'])
+def state_list(request):
+    states = State.objects.all()
+    serializer = StateSerializer(states, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def state(request, pk):
+    state_obj = get_object_or_404(State, pk=pk)
+    serializer = StateSerializer(state_obj)
+    return Response(serializer.data)
+
